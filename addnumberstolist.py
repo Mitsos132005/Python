7d740bc626c8ea5d21d5a9e341fd3e57dce64553
Mitsos132005 <dimitrios.psaroudakis@gmail.com>
2020-11-02 21:48:49 +0100

    exercises

---
+++ addnumberstolist.py
@@ -0,0 +1,9 @@
+list1 = [1,2,3]
+list1.append(71)
+list1.append(72)
+list1.append(73)
+print(list1)
+list1.insert(3,4)
+list1.insert(4,5)
+list1.insert(5,6)
+print(list1)
--- printmaxlist.py
+++ printmaxlist.py
@@ -1,2 +1,4 @@
-list1 = [12,34,56,78,90]
-print(max(list1))
+list1 = ['12','34','56','78','90']
+largest = list1[0]
+while True:
+    
